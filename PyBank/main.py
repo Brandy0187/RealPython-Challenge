2a6fa67e92d8cc51f98f6d2e5556406e88c1628f
Brandy0187 <122852100+Brandy0187@users.noreply.github.com>
2023-02-24 13:01:04 -0500

    PyBank/main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,29 @@
 import os
 import csv
-budget_csv = os.path.join(".", "Module 03 Challenge", "PyBank", "Resouces", "budget_data.csv")
+
+budget_csv = os.path.join("budget_data.csv")
 
  #open and read csv
 with open(budget_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-numMonths = set()
+    #print(csv_reader)
+    
 
-    #Reading the csv file
-csv_header = next(csv_file)
-total = 0
+    #Reading the csv file and ignore the header
+    csv_header = next(csv_file)
+    numMonths = set()
 
-    #Counting the month/date row
-for row in csv_reader:
-    numMonths.add(row[0])
-print(len(numMonths))
+     #Counting the month/date row
+    for row in csv_reader:
+        numMonths.add(row[0])
+    #print (len(numMonths))
 
-    #Count total profit/loss
-for row in csv.reader(csv_file):
-    total += int(row[1])
-print (total)
+#Calculate the total profit/loss
+    pft_loss = (float (row[1]) for row in csv_reader)
+    total = sum(pft_loss)
+    print (total)
 
-#identify the change in profit/loss and return avg
+#
+# #identify the change in profit/loss and return avg
 
-#find per month total
+
